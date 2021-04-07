8066a4b1fdafda77706650ba17c85231634e3f1e
phattarachai <phatchai28@gmail.com>
2021-04-07 10:01:46 +0700

    add github link

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 let mix = require('laravel-mix');
 
 mix
+    .setPublicPath('public')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('@tailwindcss/jit'),
